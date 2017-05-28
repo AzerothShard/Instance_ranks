d86b7a08520edc460be6b4511c98cb83b538260a
Stefano Borzì <stefanoborzi32@gmail.com>
2017-05-28 23:29:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   var app = angular.module('instance_ranks');
 
   /* Edit with path of APAW */
-  app.api = "http://azerothshard.org/modules/apaw/web/drsl_azs/";
+  app.api = "https://azerothshard.org/modules/apaw/web/drsl_azs/";
 
   // take "connections" from app/config/parameters.yml (ex. "local")
 
