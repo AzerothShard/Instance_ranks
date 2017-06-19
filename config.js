054c3925b5f674488dc654724df8b84703347269
Yehonal <yehonal.azeroth@gmail.com>
2017-06-19 18:25:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   var app = angular.module('instance_ranks');
 
   /* Edit with path of APAW */
-  app.api = "https://azerothshard.org/modules/apaw/web/drsl_azs/";
+  app.api = location.protocol+"//azerothshard.org/modules/apaw/web/drsl_azs/";
 
   // take "connections" from app/config/parameters.yml (ex. "local")
 
