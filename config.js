8a56dfdccd807ac7ae6f9608e433c60e7d1a5b9c
Helias <stefanoborzi32@gmail.com>
2017-10-26 10:30:08 +0200

    Updated config.js

--- config.js
+++ config.js
@@ -2,12 +2,10 @@
   var app = angular.module('instance_ranks');
 
   /* Edit with path of APAW */
-//  app.api = location.protocol+"//azerothshard.org/modules/apaw/web/drsl_azs/";
-  app.api = "http://azerothshard.org/modules/apaw/web/drsl_azs/";
+  app.api = location.protocol+"//azerothshard.org/modules/apaw/web/drsl_azs/";
 
   /* Edith with path of TC-JSON-API */
-//  app.tc_api = location.protocol+"//azerothshard.org/modules/TC-JSON-API/public/index.php/";
-  app.tc_api = "http://azerothshard.org/modules/TC-JSON-API/public/index.php/";
+  app.tc_api = location.protocol+"//azerothshard.org/modules/TC-JSON-API/public/index.php/";
 
   // take "connections" from app/config/parameters.yml (ex. "local")
 
